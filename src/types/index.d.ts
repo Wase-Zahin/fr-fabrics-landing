aeae168549dc73149bce960005e1aea6a804ebc6
Wase-Zahin <w.zahin@yahoo.com>
2025-07-06 03:46:55 +0600

    fix: config

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,23 @@
 declare module '*.css';
+
+declare global {
+    interface SymbolConstructor {
+        readonly iterator: unique symbol;
+        readonly toStringTag: unique symbol;
+    }
+
+    interface Iterable<T> {
+        [Symbol.iterator](): Iterator<T>;
+    }
+
+    interface Iterator<T> {
+        next(): IteratorResult<T>;
+    }
+
+    interface IteratorResult<T> {
+        done: boolean;
+        value: T;
+    }
+}
+
+export {};
